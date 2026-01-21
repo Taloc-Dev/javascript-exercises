07338f696e3e31e9975d0951a0641e4947092695
Daniel Gonzalez <219147853+Taloc-Dev@users.noreply.github.com>
2026-01-21 10:58:20 +0100

    fix: Prevent negative ints as arguments

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,7 +1,10 @@
 const sumAll = function(a, b) {
+
+    if(a < 0 || b < 0) return "ERROR";
+
     const biggerNum = a > b ? a : b;
     const smallerNum = a < b ? a : b;
-    
+
     let sum = 0;
     for(let i = smallerNum; i <= biggerNum; i++){
         sum += i;
